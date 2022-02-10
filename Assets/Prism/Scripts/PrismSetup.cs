69d1c5f3eef173d2baba69607ce043bf31a4021b
KIBA <kjh030529@gmail.com>
2022-02-11 04:56:09 +0900

    Change Setup Defualt

--- Assets/Prism/Scripts/PrismSetup.cs
+++ Assets/Prism/Scripts/PrismSetup.cs
@@ -15,7 +15,7 @@ namespace Prism.Setup
         public Camera CaptureCam;
         public float OriginSize = 2.813095f;
         public string StartUrl = "http://localhost:8080/";
-        public bool UrlSetted = false;
+        public bool UrlSetted;
         public GameObject EventPool;
         public DataMap map;
         public string[] endPointCommand;
